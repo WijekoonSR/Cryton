7dc4ab8a795d4595603043d1e663857ebd36295f
WijekoonSR <42477588+WijekoonSR@users.noreply.github.com>
2019-08-04 13:33:01 +0530

    Added other uis

---
+++ app/src/main/java/com/application/ultimatee_bookreader/request_books_manager.java
@@ -0,0 +1,14 @@
+package com.application.ultimatee_bookreader;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class request_books_manager extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_books_manager);
+    }
+}
