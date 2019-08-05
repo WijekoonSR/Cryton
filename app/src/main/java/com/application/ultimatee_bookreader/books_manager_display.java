d9101cafa7f2a11daab68f006cd9783eb8f9ac85
WijekoonSR <42477588+WijekoonSR@users.noreply.github.com>
2019-08-05 20:03:37 +0530

    Fixed Errors

---
+++ app/src/main/java/com/application/ultimatee_bookreader/books_manager_display.java
@@ -0,0 +1,14 @@
+package com.application.ultimatee_bookreader;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class books_manager_display extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_books_manager_display);
+    }
+}
