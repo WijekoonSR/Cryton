214e2502b00bed1a94aa6f00d6162d186d1ea8ef
WijekoonSR <42477588+WijekoonSR@users.noreply.github.com>
2019-09-12 15:00:36 +0530

    changed Dashboard interfaces

--- app/src/main/java/com/application/ultimatee_bookreader/DashBoard.java
+++ app/src/main/java/com/application/ultimatee_bookreader/DashBoard.java
@@ -5,109 +5,124 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-
-import com.application.ultimatee_bookreader.Avinash.user_manager_view_acc;
-import com.application.ultimatee_bookreader.Avinash.user_manager_view_privacy;
-import com.application.ultimatee_bookreader.Avinash.user_manager_view_securtity;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class DashBoard extends AppCompatActivity {
 
-    Button button;
+    ImageButton b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
 
-        button = (Button)findViewById(R.id.btnBooks);
-        button.setOnClickListener(new View.OnClickListener() {
+        Toast.makeText(getApplicationContext(), "Hello", Toast.LENGTH_LONG).show();
+
+//        b1 = findViewById(R.id.imgbtnUser);
+//        b1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent i = new Intent(DashBoard.this,user_manager_view_acc.class);
+//                startActivity(i);
+//            }
+//        });
+
+        b1 = findViewById(R.id.imgbtnViewBooks);
+        b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(DashBoard.this,BooksManager.class);
                 startActivity(i);
-                //hhh
             }
         });
 
-        button = (Button)findViewById(R.id.btnAddFeedbacks);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(DashBoard.this,feedback_manager.class);
-                startActivity(i);
-                //hhh
-            }
-        });
 
-        button = (Button)findViewById(R.id.btnViewfeedbacks);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(DashBoard.this,feedback_manager.class);
-                startActivity(i);
-                //hhh
-            }
-        });
 
-        button = (Button)findViewById(R.id.btnUserAcc);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(DashBoard.this, user_manager_view_acc.class);
-                startActivity(i);
-                //hhh
-            }
-        });
+//        b1 = findViewById(R.id.imgbtnAddBook);
+//        b1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent i = new Intent(DashBoard.this,ViewBooks.class);
+//                startActivity(i);
+//            }
+//        });
 
-        button = (Button)findViewById(R.id.btnReqBooks);
-        button.setOnClickListener(new View.OnClickListener() {
+        b1 = findViewById(R.id.imgbtnRequestBooks);
+        b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(DashBoard.this,request_books_manager.class);
                 startActivity(i);
-                //hhh
             }
         });
-
-        button = (Button)findViewById(R.id.btnPrivacy);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(DashBoard.this, user_manager_view_privacy.class);
-                startActivity(i);
-                //hhh
-            }
-        });
-
-        button = (Button)findViewById(R.id.btnSecurity);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(DashBoard.this, user_manager_view_securtity.class);
-                startActivity(i);
-                //hhh
-            }
-        });
-
-        button = (Button)findViewById(R.id.btnReqBooks);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(DashBoard.this,request_books_manager.class);
-                startActivity(i);
-                //hhh
-            }
-        });
-
-        button = (Button)findViewById(R.id.btnViewfeedbacks);
-        button.setOnClickListener(new View.OnClickListener() {
+        b1 = findViewById(R.id.imgbtnFeedBacks);
+        b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(DashBoard.this,feedback_manager.class);
                 startActivity(i);
-                //hhh
+            }
+        });
+
+//        b1 = findViewById(R.id.imgbtnBookmarks);
+//        b1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent i = new Intent(DashBoard.this,ViewBooks.class);
+//                startActivity(i);
+//            }
+//        });
+
+//        b1 = findViewById(R.id.imgbtnPrivacy);
+//        b1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent i = new Intent(DashBoard.this,user_manager_view_privacy.class);
+//                startActivity(i);
+//            }
+//        });
+//
+//        b1 = findViewById(R.id.imgbtnLogOut);
+//        b1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent i = new Intent(DashBoard.this,MainActivity.class);
+//                startActivity(i);
+//            }
+//        });
+
+        b1 = findViewById(R.id.imgbtnExit);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                System.exit(0);
             }
         });
 
     }
+
+//    public void startActivity(View v){
+//        switch (v.getId()){
+//            case R.id.imgbtnBiography :
+//                Intent i = new Intent(DashBoard.this,BooksManager.class);
+//                startActivity(i);
+//                break;
+//            case R.id.imgbtnFantasy :
+//                break;
+//            case R.id.imgbtnHorror :
+//                break;
+//            case R.id.imgbtnMystery :
+//                break;
+//            case R.id.imgbtnRomance :
+//                break;
+//            case R.id.imgbtnScience :
+//                break;
+//            case R.id.imgbtnTextile :
+//                break;
+//            case R.id.imgbtnWestern :
+//                break;
+//        }
+
+//    }
 }
