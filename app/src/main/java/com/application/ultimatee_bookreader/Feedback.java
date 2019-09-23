2eb4ef3113eef40bfd2a3ba1f618562c32a49687
WijekoonSR <42477588+WijekoonSR@users.noreply.github.com>
2019-09-23 14:23:09 +0530

    Fixed errors on feedback add

---
+++ app/src/main/java/com/application/ultimatee_bookreader/Feedback.java
@@ -0,0 +1,45 @@
+package com.application.ultimatee_bookreader;
+
+public class Feedback {
+    String name;
+    String email;
+    String text;
+    String type;
+
+
+    public Feedback() {
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+
+
+}
